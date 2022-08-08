e144d7777d05617fd2206daecf359a5884f0ecaa
Jie <y1020329@gmail.com>
2022-08-08 23:02:12 +0800

    first commit

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,6 @@
-console.log('Hello!');
-
-$(document).ready(() => {
-  console.log('HesSchool Hello!');
+$(document).ready(function() {
+  $('.showmenu').on('click',  function(e){
+     e.preventDefault();
+     $('body').toggleClass('menu-show');
+ });
 });
